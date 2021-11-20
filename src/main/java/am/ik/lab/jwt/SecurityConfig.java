c82546fef5a4be77bac1ef45d16d3ac45c2f02f6
Toshiaki Maki <tmaki@vmware.com>
2021-11-20 13:37:21 +0900

    Bump to Spring Boot 2.6.0

--- src/main/java/am/ik/lab/jwt/SecurityConfig.java
+++ src/main/java/am/ik/lab/jwt/SecurityConfig.java
@@ -15,11 +15,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests ->
                 authorizeRequests
-                        .mvcMatchers("oauth/token").permitAll()
-                        .antMatchers(HttpMethod.GET, "todos/**").hasAuthority("SCOPE_todo:read")
-                        .antMatchers(HttpMethod.POST, "todos/**").hasAuthority("SCOPE_todo:write")
-                        .antMatchers(HttpMethod.PUT, "todos/**").hasAuthority("SCOPE_todo:write")
-                        .antMatchers(HttpMethod.DELETE, "todos/**").hasAuthority("SCOPE_todo:write")
+                        .mvcMatchers("/oauth/token").permitAll()
+                        .antMatchers(HttpMethod.GET, "/todos/**").hasAuthority("SCOPE_todo:read")
+                        .antMatchers(HttpMethod.POST, "/todos/**").hasAuthority("SCOPE_todo:write")
+                        .antMatchers(HttpMethod.PUT, "/todos/**").hasAuthority("SCOPE_todo:write")
+                        .antMatchers(HttpMethod.DELETE, "/todos/**").hasAuthority("SCOPE_todo:write")
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(r -> r.jwt())
                 .csrf(csrf -> csrf.disable())
